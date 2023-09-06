af40d191e17fb123f881f9339e3296e7943b8dce
Your Name <deckerwill7@gmail.com>
2023-09-06 10:49:06 -0500

    read in indices from R and assign words

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin env python3
+
+##### Assigning words to transitions created in main.R #####
+
+# imports
+import pandas as pd
+from itertools import chain
+
+# create pandas df and extrapolate indices
+df = pd.read_csv('indices.csv')
+wordidx = list(df["x"])
+
+# word variables
+w1 = ["pi", "tu", "bi"]
+w2 = ["bu", "pa", "da"]
+w3 = ["di", "ba", "pu"]
+w4 = ["ta", "ti", "tu"]
+
+# create empty list
+wordlist = []
+
+# replace 
+for i in wordidx:
+    if wordidx[i] == 1:
+        wordlist.append(w1)
+    elif wordidx[i] == 2:
+        wordlist.append(w2)
+    elif wordidx[i] == 3:
+        wordlist.append(w3)
+    elif wordidx[i] == 4:
+        wordlist.append(w4)
+
+# unlist and write to csv
+wordlist = pd.DataFrame(list(chain.from_iterable(wordlist)))
+wordlist.to_csv('wordlist.csv')
